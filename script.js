e7415cf296924dd1381f1bebb1583e11e919b2b4
anoynomou <76471265+anoynomou@users.noreply.github.com>
2022-05-12 13:16:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ const server = {
     iceServers:[
         {
             urls:[
-                'stun:stun.l.google.com:19302',
-                'stun:stun1.l.google.com:19302',
-                'stun:stun2.l.google.com:19302',
-                'stun:openrelay.metered.ca:80',
+                    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    }
             ]
         }
     ]
