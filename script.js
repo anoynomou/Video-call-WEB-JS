9f10c3616052194512b2afb8d64f0211ef843894
anoynomou <76471265+anoynomou@users.noreply.github.com>
2022-05-12 13:09:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ let PeerConnection;
 const server = {
     iceServers:[
         {
-            urls:['stun:stun1.l.google.com:19302','stun:stun2.l.google.com:19302']
+            urls:[
+                'stun:stun.l.google.com:19302',
+                'stun:stun1.l.google.com:19302',
+                'stun:stun2.l.google.com:19302',
+                'stun:openrelay.metered.ca:80',
+            ]
         }
     ]
 }
