d11044ad90b50a08ce7228691ac57dab5435cb69
anoynomou <76471265+anoynomou@users.noreply.github.com>
2022-07-23 23:28:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -12,13 +12,7 @@ const server = {
        },
        {
           urls:"stun:stun1.l.google.com:19302"
-       },
-       
-       {
-        url: 'turn:192.158.29.39:3478?transport=tcp',
-    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-    username: '28224511:1379330808'
-}
+       }
 
        
     ]
